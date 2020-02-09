63b688411267053b7e38574225eca247ec430e73
Marcelo Barboza <marcelo.barboza.cmkp@gmail.com>
2020-02-08 21:01:34 -0300

    Primeiro commit, posteriormento colocarei um leia-me, porem o projeto esta todo comentado

---
+++ cmkpmoney-api/src/main/java/com/cmkpmoney/api/service/PessoaService.java
@@ -0,0 +1,36 @@
+package com.cmkpmoney.api.service;
+
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.stereotype.Service;
+
+import com.cmkpmoney.api.model.Pessoa;
+import com.cmkpmoney.api.repository.PessoaRepository;
+
+@Service
+public class PessoaService {
+
+	@Autowired
+	private PessoaRepository pessoaRepository;
+	
+	public Pessoa atualizar(Long id, Pessoa pessoa) {
+		
+		Pessoa pessoaSalva = buscarPessoalSalvaPeloCodigo(id);
+		BeanUtils.copyProperties(pessoa, pessoaSalva, "codigo");
+		
+		return pessoaRepository.save(pessoaSalva);		
+	}	
+
+	public void atualizarPropriedadeAtivo(Long codigo, Boolean ativo) {
+		Pessoa pessoaSalva = buscarPessoalSalvaPeloCodigo(codigo);
+		pessoaSalva.setAtivo(ativo);
+		pessoaRepository.save(pessoaSalva);
+	}
+	
+	public Pessoa buscarPessoalSalvaPeloCodigo(Long id) {
+		Pessoa pessoaSalva = pessoaRepository.findById(id)
+				.orElseThrow(() -> new EmptyResultDataAccessException(1));
+		return pessoaSalva;
+	}
+}
---
+++ cmkpmoney-api/src/main/java/com/cmkpmoney/api/service/exception/PessoaInexistenteOuInativaException.java
@@ -0,0 +1,6 @@
+package com.cmkpmoney.api.service.exception;
+
+public class PessoaInexistenteOuInativaException extends RuntimeException {
+	private static final long serialVersionUID = 1L;
+
+}
